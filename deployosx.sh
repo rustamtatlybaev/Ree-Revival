c04c5f364270fa854457ba1cbb06134ba6dca459
Ree New Team <38903436+ReeCoin2@users.noreply.github.com>
2018-05-04 13:16:10 +0200

    change QT mac name

--- deployosx.sh
+++ deployosx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OSX_DEPLOY_SCRIPT=contrib/macdeploy/macdeployqtplus
-OSX_APP=Bitcoin_Lightning-Qt.app
+OSX_APP=Ree-Qt.app
 OSX_QT_TRANSLATIONS=da,de,es,hu,ru,uk,zh_CN,zh_TW,fr
 OSX_FANCY_PLIST=contrib/macdeploy/fancy.plist
 
